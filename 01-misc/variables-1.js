c7fed654f5f91b251f37137dbf8ff315ed9f0cf5
shuresm57 <vsorensen31@gmail.com>
2026-01-29 10:30:49 +0100

    crud

---
+++ 01-misc/variables-1.js
@@ -0,0 +1,39 @@
+// nodejs REPL
+
+// type coersion
+// strict quality
+
+
+// use const to declare a variable that won't change
+
+// const is sonstant in the assignment
+const luckynumber = 4;
+
+console.log(luckynumber); 
+
+// JS Data types
+// String, Number, Boolean, Object, BigInt, Symbol, Null, Undefined
+
+// For at ændre en const, skal det defineres som et Object
+// Forskellen på et javascript object og json er at der skal være
+// quotation marks i key også man kan heller ikke skrive kommentarer
+
+const person = {
+    //key value pair
+    name: "mathias"
+}
+
+person.name = "Gustav";
+
+person.age = 123097812;
+
+console.log(person);
+
+
+const people = [];
+
+people.push("Victor");
+
+console.log(people);
+
+// ASI = Automatic Semicolon Insertion
