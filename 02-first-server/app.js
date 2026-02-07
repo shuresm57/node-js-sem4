a00d45123db6d0ca54154cea01404a175dff5abd
shuresm57 <vsorensen31@gmail.com>
2026-02-07 12:51:34 +0100

    started documentation

--- 02-first-server/app.js
+++ 02-first-server/app.js
@@ -1,33 +1,34 @@
 // import express
-const express = require('express')
+const express = require('express');
 // instantiate express
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ data: "Welcome to min minecraft server!" })
+  res.send({ data: 'Welcome to min minecraft server!' });
 });
 
 app.get('/snowstorm', (req, res) => {
-    res.send({ data: "snestorm kl 12!!!!" })
+  res.send({ data: 'snestorm kl 12!!!!' });
 });
-
 
 // how can we send data in a GET request?
 
-// path variable 
+// path variable
 app.get('/cars/:carModel', (req, res) => {
-    console.log(req.params);
-    res.send({ data: `Your ${req.params.carModel} is very nice` });
-})
+  console.log(req.params);
+  res.send({ data: `Your ${req.params.carModel} is very nice` });
+});
 
 app.get('/cars/:carModel/:year', (req, res) => {
-    console.log(req.params);
-    res.send({ data: `Your ${req.params.carModel} from the year ${req.params.year} is very nice` });
-})
+  console.log(req.params);
+  res.send({
+    data: `Your ${req.params.carModel} from the year ${req.params.year} is very nice`,
+  });
+});
 
 // query string / query parameters
-app.get('/bag', (req, res) =>{
-    res.send({ data: req.query })
-})
+app.get('/bag', (req, res) => {
+  res.send({ data: req.query });
+});
 
 app.listen(8080);
