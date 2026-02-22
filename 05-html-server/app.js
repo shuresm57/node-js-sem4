4db72824b099563542ea0f1136ab51f73ce1c219
shuresm57 <vsorensen31@gmail.com>
2026-02-22 20:22:40 +0100

    finished time server assignment

--- 05-html-server/app.js
+++ 05-html-server/app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/frontpage.html');
-})
+  res.sendFile(__dirname + '/public/frontpage.html');
+});
 
-//listen
+// listen
 app.listen(8080, (error) => {
-    if (error) {
-        console.log("Error starting the server");
-        return;
-    }
-    console.log("Server is running on port", 8080)
+  if (error) {
+    console.log('Error starting the server');
+    return;
+  }
+  console.log('Server is running on port', 8080);
 });
