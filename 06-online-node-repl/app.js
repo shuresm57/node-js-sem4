3d829a7d7b5c6fdfd109761283794943554074fa
shuresm57 <vsorensen31@gmail.com>
2026-03-05 10:57:38 +0100

    made replUtil and importen water.css

--- 06-online-node-repl/app.js
+++ 06-online-node-repl/app.js
@@ -17,16 +17,31 @@ app.get('/about', (req, res) => {
 
 // ====================== API ==============================
 
+import { getOrCreateSandboxContext, executeCodeInSandbox } from './util/replUtil.js';
+
 app.post('/api/repl', (req, res) => {
-    let replCode = req.body?.replCode;
+    // let replCode = req.body?.replCode;
+
+    if(!req.body) {
+      return res.status(400).send({ errorMessage: 'Missing a JSON body' });
+    }
+
+    const {replCode, sandBoxID } = req.body;
 
     if (!replCode) {
         return res.status(400).send({ errorMessage: 'Missing the key replCode in the JSON body' })
     }
 
-    replCode = replCode.replace('console.log("', '').replace('")', '');
+    const sandbox = getOrCreateSandboxContext(sandBoxID);
 
-    res.send({ data: replCode });
+    const { error, success, output, result } = executeCodeInSandbox(sandbox, replCode);
+
+
+    if(error) {
+      return res.status(500).send({ errorMessage: 'Error executing the provided code'});
+    }
+
+    res.send({ data: { success, output, result } });
 })
 
 // this is the process node runs from
@@ -38,7 +53,7 @@ const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, (error) => {
   if (error) {
     // we can call on the return value for the const server, not correct to use, but most truthful
-    console.log('Could not start the server on', server.address().port);
+    console.log('Could not start the server on', error.message);
   };
 
   console.log('Server running on ', server.address().port);
