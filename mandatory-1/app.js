54325b06a1bb08bdfb4cf804b866b3113f8eb6be
Valdemar Støvring Sørensen <155188800+shuresm57@users.noreply.github.com>
2026-03-11 14:24:09 +0100

    Created relevant get methods for doc pages

--- mandatory-1/app.js
+++ mandatory-1/app.js
@@ -2,21 +2,34 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-app.use(express.static('public', {
-  extensions: ['html']
-}));
-
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-  res.sendFile(path.resolve('public/frontpage/frontpage.html'));
+  res.sendFile(path.resolve('public/frontpage/frontpage'));
 });
 
 app.get('/week-1', (req,res) => {
-  res.sendFile(path.resolve('public/documentation/week-1.html'));
+  res.sendFile(path.resolve('public/documentation/week-1'));
 });
 
 app.get('/week-2', (req,res) => {
-  res.sendFile(path.resolve('public/documentation/week-2.html'));
+  res.sendFile(path.resolve('public/documentation/week-2'));
+});
+
+app.get('/week-3', (req,res) => {
+  res.sendFile(path.resolve('public/documentation/week-3'));
+});
+
+app.get('/week-4', (req,res) => {
+  res.sendFile(path.resolve('public/documentation/week-4'));
+});
+
+app.get('/week-5', (req,res) => {
+  res.sendFile(path.resolve('public/documentation/week-5'));
+});
+
+app.get('/week-6', (req,res) => {
+  res.sendFile(path.resolve('public/documentation/week-6'));
 });
 
 app.listen(8080, (error) => {
@@ -26,4 +39,3 @@ app.listen(8080, (error) => {
 
   console.log('Server running on ', 8080);
 });
-
