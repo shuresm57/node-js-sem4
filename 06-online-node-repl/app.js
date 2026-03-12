dbb00669c08943e0da73b2145bc4184af6f15f67
shuresm57 <vsorensen31@gmail.com>
2026-03-12 09:44:56 +0100

    created router handling in 06

--- 06-online-node-repl/app.js
+++ 06-online-node-repl/app.js
@@ -17,36 +17,9 @@ app.get('/pages/about', (req, res) => {
 
 // ====================== API ==============================
 
-import { getOrCreateSandboxContext, executeCodeInSandbox } from './util/replUtil.js';
+import replRouter from './routers/replRouter.js';
 
-app.post('/api/repl', (req, res) => {
-    // let replCode = req.body?.replCode;
-
-    console.log(req.body);
-
-    if(!req.body) {
-      return res.status(400).send({ errorMessage: 'Missing a JSON body' });
-    }
-
-    const {replCode, sandBoxID } = req.body;
-
-    if (!replCode) {
-        return res.status(400).send({ errorMessage: 'Missing the key replCode in the JSON body' })
-    }
-
-    const sandbox = getOrCreateSandboxContext(sandBoxID);
-
-    const { error, success, output, result } = executeCodeInSandbox(sandbox, replCode);
-
-    if(error) {
-      return res.status(500).send({ 
-        data: { error },
-        errorMessage: 'Error executing the provided code'
-      });
-    }
-
-    res.send({ data: { success, output, result } });
-})
+app.use(replRouter);
 
 // this is the process node runs from
 // console.log(process)
