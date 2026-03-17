b5298e4d0e10505b920224998e7ea97c6f8cdb0d
Valdemar Støvring Storgaard <vss@eaea.dk>
2026-03-17 15:31:35 +0100

    added tocbot to sidebar.js

--- mandatory-1/public/assets/js/sidebar.js
+++ mandatory-1/public/assets/js/sidebar.js
@@ -1,21 +1,34 @@
 const nav = document.createElement('nav');
+const nav2 = document.createElement('nav');
 
-nav.className = 'sidebar';
+nav.className = 'sidebar2';
 nav.innerHTML = `
-  <h3>Topics</h3>
-  <a href="./week-1">Introduction to Node.js</a>
-  <a href="./week-2">First Server</a>
-  <a href="./week-3">HTTP & Loops</a>
-  <a href="./week-4">Time, Fetch & Deployment</a>
-  <a href="./week-5">Modern Node.js</a>
-  <a href="./week-6">Development Basics</a>
-  <a href="./week-7">Advanced Node.js</a>
+  <h1>Topics</h1>
+  <a href="./week-1"> Week 1 - Introduction to Node.js</a>
+  <a href="./week-2"> Week 2 - First Server</a>
+  <a href="./week-3"> Week 3 - HTTP & Loops</a>
+  <a href="./week-4"> Week 4 - Time, Fetch & Deployment</a>
+  <a href="./week-5"> Week 5 - Modern Node.js</a>
+  <a href="./week-6"> Week 6 - Development Basics</a>
+  <a href="./week-7"> Week 7 - Advanced Node.js</a>
 `;
-
 
 document.body.appendChild(nav);
 
-const topbarHeight = document.querySelector('.topbar').offsetHeight;
-window.addEventListener('scroll', () => {
-  nav.style.top = `${Math.max(1, topbarHeight - window.scrollY)}px`;
+nav2.className = 'sidebar';
+nav2.innerHTML = `
+  <h3>In this article</h3>
+  <div class="js-toc"></div>
+`;
+document.body.appendChild(nav2);
+
+tocbot.init({
+  tocSelector: '.js-toc',
+  contentSelector: 'main',
+  headingSelector: 'h2',
+  hasInnerContainers: true,
+  smoothScroll: false,
+  headingsOffset: 0,  
 });
+
+console.log(tocbot);
