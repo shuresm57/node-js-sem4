0687633b9c04ee39456b01cd86ac95f26443430c
shuresm57 <vsorensen31@gmail.com>
2026-04-12 22:06:11 +0200

    added the autheticateJWT method

--- mandatory-2/server/middleware/jwtAuthenticator.js
+++ mandatory-2/server/middleware/jwtAuthenticator.js
@@ -1,15 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+
+// TODO: Use in routes that need protection
 const authenticateJWT = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1];
 
   if (!token) {
-    return res.status(401).send('Access Denied');
+    return res.status(401).send({ error: 'Access Denied' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(401).send('Invalid Token');
+    if (err) return res.status(401).send({ error: 'Invalid Token' });
 
     req.user = user;
-
     next();
   });
 };
