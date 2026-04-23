3174bfabbf436b2ee65dbd6ef27057e36d9aefa9
shuresm57 <vsorensen31@gmail.com>
2026-04-23 22:25:56 +0200

    started the exam project

---
+++ _eksamen/server/app.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+
+import authRouter from './routers/authRouter.js';
+
+
+const app = express();
+app.use(express.json);
+app.use(cookieParser);
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+app.use(helmet);
+app.use(generalLimiter);
+app.use(authLimiter);
+
+const PORT = process.env.PORT ?? 8080;
+
+app.listen(PORT);
+
+console.log('Server running at ', PORT);
